MP2E/kexplus

commit 0fb1111bf3d8d3a27769273059fa72f5cda7a7e5

import shoot-while-savnig fix from kex

src/g_actions.c
@@ -418,12 +418,17 @@ dboolean G_ActionResponder(event_t *ev)
             break;
 
         TryActions(KeyActions[ev->data1], ev->type == ev_keyup);
+        break;
 
+    // villsa 12/20/2013: properly handle mouse button actions and
+    // handle mouse movement in its own event state
+    case ev_mousedown:
+    case ev_mouseup:
+        ProcessButtonActions(MouseActions, ev->data1, MouseButtons);
+        MouseButtons = ev->data1;
         break;
 
     case ev_mouse:
-        ProcessButtonActions(MouseActions, ev->data1, MouseButtons);
-        MouseButtons = ev->data1;
         G_DoCmdMouseMove(ev->data2, ev->data3);
         break;
 

commit ecc1adaf10c1abd6c5bd221961fd79df32617219

Fix regression in mouse code introduced in 0fb1111

src/g_actions.c
@@ -422,12 +422,11 @@ dboolean G_ActionResponder(event_t *ev)
 
     // villsa 12/20/2013: properly handle mouse button actions and
     // handle mouse movement in its own event state
+    // MP2E 12/10/2013: ev_mousedown and mouseup need G_DoCmdMouseMove
     case ev_mousedown:
     case ev_mouseup:
         ProcessButtonActions(MouseActions, ev->data1, MouseButtons);
         MouseButtons = ev->data1;
-        break;
-
     case ev_mouse:
         G_DoCmdMouseMove(ev->data2, ev->data3);
         break;