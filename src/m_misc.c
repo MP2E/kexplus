7f28c1e01a93c5b41c166eca0cc2cd589ebd4602
dotfloat <dotfloat@gmail.com>
2014-04-05 16:57:30 +0200

    Replace gluScaleImage with GL_Pixmap in savegame thumbnail.

--- src/m_misc.c
+++ src/m_misc.c
@@ -47,6 +47,7 @@
 #include "g_local.h"
 #include "st_stuff.h"
 #include "i_png.h"
+#include "gl_pixmap.h"
 #include "gl_texture.h"
 #include "p_saveg.h"
 
@@ -328,15 +329,18 @@ void M_ScreenShot(void)
 
 int M_CacheThumbNail(byte ** data)
 {
+	dpixmap *pm;
 	byte *buff;
 	byte *tbn;
 
 	buff = GL_GetScreenBuffer(0, 0, video_width, video_height);
 	tbn = Z_Calloc(SAVEGAMETBSIZE, PU_STATIC, 0);
 
-	gluScaleImage(GL_RGB, video_width, video_height,
-		      GL_UNSIGNED_BYTE, buff, 128, 128, GL_UNSIGNED_BYTE, tbn);
+	pm = GL_PixmapCreate(video_width, video_height, DPM_PFRGB, buff);
+	GL_PixmapScaleTo(pm, NULL, 128, 128);
+	dmemcpy(tbn, pm->map, SAVEGAMETBSIZE);
 
+	GL_PixmapFree(pm);
 	Z_Free(buff);
 
 	*data = tbn;
